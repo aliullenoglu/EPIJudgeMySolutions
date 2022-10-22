5c58e58ddebb33e6e318f3357c52a4f9c42a3937
Ali Ullenoglu <aliullenoglu@gmail.com>
2022-10-22 19:41:23 +0300

    5.1 finished

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,10 +1,20 @@
 #include "test_framework/generic_test.h"
+using namespace std;
+
 short Parity(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  
+  short parity = 0;
+
+  while (x > 0) {
+    x = x & (x - 1); // clear the lowest bit
+    parity++;
+  }
+
+  return parity % 2;
 }
 
 int main(int argc, char* argv[]) {
+
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "parity.cc", "parity.tsv", &Parity,
